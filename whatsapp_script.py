dbdd757de3eeb556b539c642c427a0c012d3559e
Nathan <nathanvdmxd@gmail.com>
2026-09-15 18:28:14 +0200

    deleteing useless files

--- whatsapp_script.py
+++
@@ -1,9 +0,0 @@
-import pywhatkit
-
-
-phone_numbers = ["+27799953644"]  # Add more numbers as needed
-
-for i in phone_numbers:
-    pywhatkit.sendwhatmsg_instantly(i, "HELLOW I AM UNDA DA WATA",15,True,3)
-
-print("Message sent successfully!")
